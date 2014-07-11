cd964987766d857a00081123fe462ab689bff366
jheaton5 <joel.heaton2@gmail.com>
2014-07-11 12:06:40 -0400

    Added more comments for clarity.

--- ae.c
+++ ae.c
@@ -10,15 +10,15 @@
 void title_page(void)
 {	
 char mesg[]= "A N C E S T R A L   E X P L O R A T I O N S";
-int row,col;					/* to store the number of rows and cols */
-	getmaxyx(stdscr,row,col);	/* get the number of rows and columns */
-	mvprintw((row/2)-2,(col-strlen(mesg))/2,"%s",mesg);  /* print the message at the center of the screen */
-	refresh();					/* Print it on to the real screen */
-	getch();					/* Wait for user input */
+int row,col;													/* to store the number of rows and cols */
+	getmaxyx(stdscr,row,col);									/* get the number of rows and columns */
+	mvprintw((row/2)-2,(col-strlen(mesg))/2,"%s",mesg);  		/* print the message at the center of the screen */
+	refresh();													/* Print it on to the real screen */
+	getch();													/* Wait for user input */
 	clear();
 }	
 void print_menu()
-{	char *choices[] = {
+{	char *choices[] = { 										/* array that contains the menu items */
             "Modify the Person Data.       ",
 			"Modify the Marriage Data.     ",
 			"Browse the Data in the Files. ",
@@ -26,12 +26,11 @@ void print_menu()
 			"Print a Family Group Sheet.   ",
 			"Print a Pedigree Chart.       ",
 			"Exit the Program.             ",
-           
-                  };
+            };
 
-	ITEM **my_items;
+	ITEM **my_items;											/* pointer to a pointer to the menu items */
 	int c;				
-	MENU *my_menu;
+	MENU *my_menu;												/* pointer to the menu block */
 	int n_choices, i;
 	initscr();
 	cbreak();
@@ -39,43 +38,48 @@ void print_menu()
 	keypad(stdscr, TRUE);
 	
 	n_choices = ARRAY_SIZE(choices);
-	my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
+	my_items = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *)); 	/* allocate memory for the size of the whole menu */
 
-	for(i = 0; i < n_choices; ++i)
-	        my_items[i] = new_item(choices[i], choices[i]);
-	my_items[n_choices] = (ITEM *)NULL;
+	for(i = 0; i < n_choices; ++i)								/* loop through the menu items */
+	        my_items[i] = new_item(choices[i], choices[i]); 	/* set up the menu items with new menu */
+	my_items[n_choices] = (ITEM *)NULL;							/* set the last item to NULL */
 
-	my_menu = new_menu((ITEM **)my_items);
-	mvprintw(LINES - 2, 0, "F2 to Exit");
-	post_menu(my_menu);
-	refresh();
+	my_menu = new_menu((ITEM **)my_items);						/* create the menu */
+	mvprintw(LINES - 2, 0, "F2 to Exit");						/* print the Exit command */
+	post_menu(my_menu);											/* post the menu to the hidden screen */
+	refresh();													/* make the hidden screen active */
 
-	while((c = getch()) != KEY_F(2))
+	while((c = getch()) != KEY_F(2))							/* loop through the menu choices */
 	{   switch(c)
 	    {	case KEY_DOWN:
-		        menu_driver(my_menu, REQ_DOWN_ITEM);
+		        menu_driver(my_menu, REQ_DOWN_ITEM);			/* move down one line */
 				break;
 			case KEY_UP:
-				menu_driver(my_menu, REQ_UP_ITEM);
+				menu_driver(my_menu, REQ_UP_ITEM);				/* move up one line */
 				break;
 		}
 	}	
 
-	free_item(my_items[0]);
+	free_item(my_items[0]);										/* clear the menu items and return */
 	free_item(my_items[1]);
+	free_item(my_items[2]);
+	free_item(my_items[3]);
+	free_item(my_items[4]);
+	free_item(my_items[5]);
+	free_item(my_items[6]);
 	free_menu(my_menu);
 	endwin();
 
 }
 
-int main()
+int main()														/* Maint Program */
 {	
-	initscr();					/* Start curses mode 		  */
+	initscr();													/* Start curses mode */
 	curs_set(0);
-	title_page();
-	print_menu();
+	title_page();												/* Print the Title Page */
+	print_menu();												/* Print the Menu */
 	endwin();
 
-	return 0;
+	return 0;													/* Program terminates */
 }
 
